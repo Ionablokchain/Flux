084f99406bc539f5b7162e304f6457ed736e6cda
Eric-Octavian <ericbulai@gmail.com>
2026-05-15 16:36:25 +0200

    Delete hardware/rtl directory

--- hardware/rtl/paradox_unit.v
+++
@@ -1,4 +0,0 @@
-// Hardware paradox accelerator – placeholder
-module paradox_unit (input [7:0] seed, output [31:0] paradox_code);
-    assign paradox_code = seed * 1103515245 + 12345;
-endmodule
--- hardware/rtl/temporal_clock.v
+++
@@ -1,7 +0,0 @@
-// Causal clock generator – placeholder
-module temporal_clock (
-    output reg causal_time
-);
-    initial causal_time = 0;
-    always #10 causal_time = causal_time + 1;
-endmodule
